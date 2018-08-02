1ee6a76c54e307fab973a2229077cc6f5a596f34
Janmajayamall <janmajaya.choithram@gmail.com>
2018-08-03 02:36:40 +0530

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-
+var {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose.js');
 var {Todo} = require('./models/todo.js');
 var {Users} = require('./models/user.js');
@@ -27,6 +27,22 @@ app.get('/todos', (req, res)=>{
   }, (err) =>{
     res.send(err);
   });
+});
+
+
+app.get('/todos/:id', (req, res)=>{
+  var id = req.params.id;
+
+  if(!ObjectID.isValid(id)){
+      return res.status(400).send();
+  }
+
+  Todo.findById(id).then((todo)=>{
+    if(!todo){
+      return res.status(404).send();
+    }
+    res.send({todo});
+  }).catch((e)=>{res.status(404).send()});
 });
 
 app.listen(3000, ()=>{
