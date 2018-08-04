4f590beb289bb17cce6bcb9064b50ea1c6356f14
Janmajayamall <janmajaya.choithram@gmail.com>
2018-08-04 23:49:13 +0530

    Deploying to Heroku

--- server/server.js
+++ server/server.js
@@ -1,11 +1,15 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var {ObjectID} = require('mongodb');
-var {mongoose} = require('./db/mongoose.js');
-var {Todo} = require('./models/todo.js');
-var {Users} = require('./models/user.js');  
+const express = require('express');
+const _ = require('lodash');
+const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
+const {mongoose} = require('./db/mongoose.js');
+const {Todo} = require('./models/todo.js');
+const {Users} = require('./models/user.js');
 
 var app = express();
+
+  const port = process.env.PORT || 3000;
+
 
 app.use(bodyParser.json());
 
@@ -45,8 +49,51 @@ app.get('/todos/:id', (req, res)=>{
   }).catch((e)=>{res.status(404).send()});
 });
 
-app.listen(3000, ()=>{
-  console.log('Using port 3000');
+
+app.delete('/todos/:id', (req, res)=>{
+  var id = req.params.id;
+
+  if(!ObjectID.isValid(id)){
+      return res.status(404).send();
+  }
+
+  Todo.findByIdAndRemove(id).then((todo)=>{
+    if(!todo){
+      return res.status(404).send();
+    }
+      res.status(200).send({todo});
+  }).catch((e )=>{ res.status(400).send();});
+});
+
+app.patch('/todos/:id', (req, res)=>{
+  var id = req.params.id;
+  var body = _.pick(req.body, ['text', 'completed']);
+
+  if(!ObjectID(id)){
+    res.status(404).send();
+  }
+
+  if(_.isBoolean(body.completed) && body.completed){
+    body.completedAt = new Date().getTime();
+  }else{
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo)=>{
+    if(!todo){
+      return res.status(404).send();
+    }
+
+    res.status(200).send({todo});
+  }).catch((e)=>{res.status(400).send();})
+
+});
+
+
+
+app.listen(port, ()=>{
+  console.log(`Using port ${port}`);
 });
 
 module.exports = {app};
--- server/test/server.test.js
+++ server/test/server.test.js
@@ -7,11 +7,15 @@ const {Todo} = require('./../models/todo.js');
 
 const todos = [{
   _id: new ObjectID(),
-  text: 'First test'
+  text: 'First test',
+  complete: true,
+  completeAt: 444
 },
 {
   _id: new ObjectID(),
-  text: 'Second test'
+  text: 'Second test',
+  completed: false,
+  completedAt: 222
 }]
 beforeEach((done) => {
   Todo.remove({}).then(() => {
@@ -104,3 +108,86 @@ describe('GET /todos/:id', ()=>{
       .end(done);
   });
 });
+
+describe('DELETE /todos/:id', ()=>{
+  it('should return 200 and delete the matching id document', (done)=>{
+    var hexID = todos[0]._id.toHexString();
+
+    request(app)
+      .delete(`/todos/${hexID}`)
+      .expect(200)
+      .expect((res)=>{
+        expect(res.body.todo._id).toBe(hexID);
+      })
+      .end((err, res)=>{
+        if(err){
+          return done(err);
+        }
+
+        Todo.findById(hexID).then((todo)=>{
+          expect(todo).toNotExist();
+          done();
+        }).catch((e)=>done(e));
+      });
+  });
+
+  it('should return 404 code', (done)=>{
+    var id = new ObjectID();
+    request(app)
+      .delete(`/todos/${id.toHexString()}`)
+      .expect(404)
+      .end(done);
+  });
+
+  it('should return 400 code', (done)=>{
+    var id = '123'
+    request(app)
+      .delete(`/todos/${id}`)
+      .expect(404)
+      .end(done);
+  });
+});
+
+
+describe('PATCH /todos/:id', ()=>{
+  it('should update todo', (done)=>{
+    var hexID = todos[1]._id.toHexString();
+    var text = 'new text';
+
+    request(app)
+      .patch(`/todos/${hexID}`)
+      .send({
+        completed: true,
+        text
+      })
+      .expect(200)
+      .expect((res)=>{
+        expect(res.body.todo.text).toBe(text);
+        expect(res.body.todo.completed).toBe(true);
+        expect(res.body.todo.completedAt).toBeA('number');
+      })
+      .end(done);
+  });
+
+  it('should clear completedAt when complete is set to false', (done)=>{
+    var id = todos[0]._id.toHexString();
+    var text = 'new text';
+
+    request(app)
+      .patch(`/todos/${id}`)
+      .send({
+        completed: false,
+        text
+      })
+      .expect(200)
+      .expect((res)=>{
+        expect(res.body.todo.completedAt).toNotExist();
+        expect(res.body.todo.completed).toBe(false);
+        expect(res.body.todo.text).toBe(text);
+
+
+      })
+      .end(done);
+  });
+
+  });
