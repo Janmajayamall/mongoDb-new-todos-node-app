7509200679265b1ea76029d8d7351bda1b7c2575
Janmajayamall <janmajaya.choithram@gmail.com>
2018-08-05 00:50:00 +0530

    HEROKU todo final

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://janmajayamall:youaremad18@ds111192.mlab.com:11192/todoapp' || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {
   mongoose
